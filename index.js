8ac7f2fb49fa65d3631d5a7cf62763a3031416a8
Alutta <Aluttamatambo5@gmail.com>
2024-09-05 14:22:56 +0200

    added ternary operator for stock message

--- index.js
+++ index.js
@@ -6,13 +6,14 @@
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
 let drinktype = userPreference ? userPreference : "regular"; //ternary operator to check preference
-console.log(`user prefers a ${drinktype} variety`); //results
+console.log(`user prefers a ${drinktype} Regular`); //results
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
+let stockmessage = cansLeft >5 ? 'time to restock':'we are stocked'// ternary operator to check for stock
 console.log(``);
 
 
@@ -25,8 +26,8 @@ console.log(`'}`);
 
 
 // #### Challenge 4: Temperature Suitability
-// Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
-// if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
+// Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!"
+// if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C. 
 
 let currentTemp = 4; // Current temperature in °C
 console.log(``);
